434fa67be3b7bd34fb09df5d0b74a6c91de87127
zack831 <rishav83@proton.me>
2023-06-05 22:02:38 +0530

    major cities fn implemnted and minor tweaks

--- javascript/home_page.js
+++ javascript/home_page.js
@@ -1,6 +1,6 @@
 class Weather{
-
 	constructor(val){
+		var test= 0;
 		const apikey = "9bb9485e113f7f8e07520d9f65878e19";
 		this.fetchLocationValues(val,apikey);
 	}
@@ -15,7 +15,8 @@ class Weather{
 			)
 		.then((response) => response.json())
 		//.then((data) => console.log(data))
-		.then((data) => this.displayDaily(data));
+		.then((data) => this.displayDaily(data))
+		// .then((data) => this.major_cities_display(data));
 	}
 
 	displayDaily(data)
@@ -37,13 +38,59 @@ class Weather{
 				document.querySelector(`.d${index}_temperature`).innerText = temp + "°C";
 				document.querySelector(`.d${index}_humidity`).innerText = "Humidity "+humidity+"%";
 				document.querySelector(`.d${index}_speed`).innerText = "Wind Speed " + speed + " Km/h";
-				//console.log(index);
+				// console.log(value);
 
 			}
 		});
 
-		console.log(data);
+		// console.log(data);
 	}
+
+	// major_cities(){
+	// 	const arr = ["kolkata","Mumbai","Delhi","Chennai","Nagaland"];
+	// 	for (var i = 0; i <= 5; i++) {
+	// 		this.fetchLocationValues(arr[i],apikey);
+	// 	}
+	// }
+
+	// major_cities_fetch(lat,lon,apikey){
+	// 	test = test+1;
+	// 		fetch("http://api.openweathermap.org/data/3.0/onecall?lat="
+	// 			+lat
+	// 			+"&lon="
+	// 			+lon
+	// 			+"&units=metric&appid="
+	// 			+apikey
+	// 		)
+	// 	.then((response) => response.json())
+	// 	.then((data) => this.major_cities_display(data,test));
+	// 	console.log(test);
+	// }
+
+	// major_cities_display(data){
+	// 	// console.log(arr);
+	// 	// console.log(data);
+
+	// 	const temp = data.daily[0].temp.day;
+	// 	const icon = data.daily[0].weather[0].icon;
+	// 	const summary = data.daily[0].summary;
+
+	// 	document.querySelector(`.city${test}_temperature`).innerText = temp + "°C";
+	// 	document.querySelector(`.city${test}_icon`).innerHTML = `<img src ="https://openweathermap.org/img/wn/${icon}.png";/>`
+	// 	document.querySelector(`.city${test}_summary`).innerText = summary;
+
+		// for (var i = 1; i <= 5; i++) {
+
+		// 	const temp = data.daily[0].temp.day;
+		// 	const icon = data.daily[0].weather[0].icon;
+		// 	const summary = data.daily[0].summary;
+
+		// 	document.querySelector(`.city${i}_temperature`).innerText = temp + "°C";
+		// 	document.querySelector(`.city${i}_icon`).innerHTML = `<img src ="https://openweathermap.org/img/wn/${icon}.png";/>`
+		// 	document.querySelector(`.city${i}_summary`).innerText = summary;
+		// 	// console.log(temp);
+		// }
+	// }
 
 	fetchLocationValues(location,apikey)
 	{
@@ -61,8 +108,9 @@ class Weather{
 	getData(data,apikey)
 	{
 		const {lat,lon} = data[0];
-		this.fetchDaily(lat,lon,apikey)
+		this.fetchDaily(lat,lon,apikey);
 		this.fetchWeather(lat,lon,apikey);
+		// this.major_cities_fetch(lat,lon,apikey);
 	}
 
 	fetchWeather(lat,lon,apikey){
@@ -85,7 +133,7 @@ class Weather{
 		const {temp,humidity} = data.main;
 		const {speed} = data.wind;
 		//console.log(name,icon,description,temp,humidity,speed);
-		//document.querySelector("body").style.backgroundImage = "url(https://source.unsplash.com/1600x900/?nature,"+description+")";
+		document.querySelector("body").style.backgroundImage = "url(https://source.unsplash.com/1600x900/?nature,"+description+")";
 		document.querySelector(`.city`).innerText = "Weather in " + name;
 		document.querySelector(".icon").innerHTML = `<img src ="https://openweathermap.org/img/wn/${icon}.png";/>`;
 		document.querySelector(".description").innerText = description;
---
+++ javascript/weather_cities.js
@@ -0,0 +1,73 @@
+class Weather_cities{
+	constructor(){
+		const apikey = "9bb9485e113f7f8e07520d9f65878e19";
+		this.major_cities(apikey);
+	}
+
+	fetchLocationValues(location,apikey,i)
+	{
+		fetch(
+			"http://api.openweathermap.org/geo/1.0/direct?q="
+			+location
+			+"&limit=1&appid="
+			+apikey
+			)
+		.then((response) => response.json())
+		.then((data) => this.getData(data,apikey,i))
+		// .then((data) => console.log(data))
+	}
+
+	getData(data,apikey,i)
+	{
+		const {lat,lon} = data[0];
+		this.major_cities_fetch(lat,lon,apikey,i);
+	}
+
+	major_cities(apikey){
+		const arr = ["NULL","kolkata","Mumbai","Delhi","Chennai","Dimapur"];
+		for (var i = 1; i <= 5; i++) {
+			this.fetchLocationValues(arr[i],apikey,i);
+		}
+	}
+
+	major_cities_fetch(lat,lon,apikey,i){
+			fetch("http://api.openweathermap.org/data/3.0/onecall?lat="
+				+lat
+				+"&lon="
+				+lon
+				+"&units=metric&appid="
+				+apikey
+			)
+		.then((response) => response.json())
+		.then((data) => this.major_cities_display(data,i));
+		// console.log(test);
+	}
+
+	major_cities_display(data,i){
+
+		console.log(data)
+
+
+		const temp = data.current.temp;
+		const icon = data.daily[0].weather[0].icon;
+		const summary = data.daily[0].summary;
+
+		document.querySelector(`.city${i}_temperature`).innerText = temp + "°C";
+		document.querySelector(`.city${i}_icon`).innerHTML = `<img src ="https://openweathermap.org/img/wn/${icon}.png";/>`
+		document.querySelector(`.city${i}_summary`).innerText = summary;
+
+		// for (var i = 1; i <= 5; i++) {
+
+		// 	const temp = data.daily[0].temp.day;
+		// 	const icon = data.daily[0].weather[0].icon;
+		// 	const summary = data.daily[0].summary;
+
+		// 	document.querySelector(`.city${i}_temperature`).innerText = temp + "°C";
+		// 	document.querySelector(`.city${i}_icon`).innerHTML = `<img src ="https://openweathermap.org/img/wn/${icon}.png";/>`
+		// 	document.querySelector(`.city${i}_summary`).innerText = summary;
+		// 	// console.log(temp);
+		// }
+	}
+}
+
+const city = new Weather_cities();
